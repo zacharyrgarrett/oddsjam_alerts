6aeb3e3ee24e64849e01f9e852b3feb15367912e
Zachary Garrett <zacharyrgarrett@gmail.com>
2022-06-26 21:16:22 -0700

    Match webscraper with new OJ site layout

--- scrape_bets.py
+++ scrape_bets.py
@@ -47,6 +47,7 @@ alert_log = dict()
 
 class Bet:
     __bet_row_elements = []
+    __bet_info_elements = []
     __top_or_bottom_bet = 0
     __min_odds = -300
     __max_odds = 300
@@ -58,18 +59,22 @@ class Bet:
         game_date=2,
         event_info=3,
         market=4,
-        bet_description=5,
-        sportsbook_info=6,
-        market_odds=7,
-        no_vig_odds=8,
-        width=9
+        bet_info=5,
+        width=6
+    )
+    __bet_element_map = dict(
+        bet_description=0,
+        book_info=1,
+        market_info=2,
+        no_vig_odds=3
     )
 
     def __init__(self, bet_row, minimal_scrape=False):
-        start_time = time.time()
+        self.oj_bet_url = ''
         self.bet_id = ''
         self.ev_percent = ''
         self.game_date = ''
+        self.game_time = ''
         self.matchup = ''
         self.sport = ''
         self.market = ''
@@ -80,10 +85,10 @@ class Bet:
         self.no_vig_odds = ''
         self.width = ''
         self.recommended_bet = 0
-        self.time_setters = time.time() - start_time
 
         # Extract attributes
-        self.__bet_row_elements = bet_row.find_elements(By.XPATH, "./td")
+        self.oj_bet_url = bet_row.get_attribute("href")
+        self.__bet_row_elements = bet_row.find_elements(By.XPATH, "./div")
         self.__get_percent()
         self.width = self.__get_inner_text("width")
 
@@ -92,22 +97,23 @@ class Bet:
 
     @staticmethod
     def __convert_date(date_string):
-        date_string = date_string[:date_string.find(" at ")]
-        date_obj = datetime.datetime.strptime(date_string, "%a, %b %d")
-        date_obj = date_obj.replace(year=datetime.date.today().year)
-        return date_obj.strftime("%#m/%#d/%Y")
+        date_obj = datetime.datetime.strptime(date_string, "%a, %b %d at %I:%M %p")
+        today = datetime.date.today()
+        if date_obj.month < today.month:
+            date_obj = date_obj.replace(year=today.year + 1)
+        else:
+            date_obj = date_obj.replace(year=today.year)
+        return date_obj.strftime("%#m/%#d/%Y"), date_obj.strftime("%H:%M:%S")
 
     def __get_inner_text(self, element_name):
         return self.__bet_row_elements[self.__element_map[element_name]].get_attribute("innerText")
 
     def __get_bet_id(self):
-        bet_link = self.__bet_row_elements[self.__element_map["bet_id"]].find_element(By.TAG_NAME, "a").get_attribute(
-            "href")
-        game_string = bet_link.split("/")[5] + self.bet_description + str(self.actual_odds)
+        game_string = self.oj_bet_url.split("/")[5] + self.bet_description + str(self.actual_odds)
         self.bet_id = abs(hash(game_string)) % (10 ** 8)
 
     def __get_percent(self):
-        percents = self.__bet_row_elements[self.__element_map["percent"]].find_elements(By.XPATH, "./a/div/span")
+        percents = self.__bet_row_elements[self.__element_map["percent"]].find_elements(By.XPATH, "./div/span")
         top = percents[0].find_element(By.XPATH, "./p").get_attribute("innerText")
         bottom = percents[1].find_element(By.XPATH, "./p").get_attribute("innerText")
 
@@ -130,34 +136,44 @@ class Bet:
         self.matchup = event_info[0].get_attribute("innerText")
         self.sport = event_info[1].get_attribute("innerText")
 
+    def __get_bet_info(self):
+        bet_sides = self.__bet_row_elements[self.__element_map["bet_info"]].find_elements(By.XPATH, "./div")
+        for div in bet_sides:
+            self.__bet_info_elements.append(div.find_elements(By.XPATH, "./div"))
+        self.__get_bet_description()
+        self.__get_sportsbook_info()
+        self.__get_market_odds()
+        self.__get_no_vig_odds()
+        self.__get_recommended_bet_size()
+        self.__get_bet_id()
+
     def __get_bet_description(self):
-        bet_sides = self.__bet_row_elements[self.__element_map["bet_description"]].find_elements(By.TAG_NAME, "p")
-        self.bet_description = bet_sides[self.__top_or_bottom_bet].get_attribute("innerText")
+        self.bet_description = self.__bet_info_elements[self.__top_or_bottom_bet][self.__bet_element_map["bet_description"]].get_attribute("innerText")
 
     def __get_sportsbook_info(self):
-        info = self.__bet_row_elements[self.__element_map["sportsbook_info"]].find_element(By.XPATH, "./span/span")
+        info = self.__bet_info_elements[self.__top_or_bottom_bet][self.__bet_element_map["book_info"]].find_element(By.XPATH, "./div")
 
         # Get Odds
-        self.actual_odds = info.find_elements(By.XPATH, "./span")[self.__top_or_bottom_bet].get_attribute("innerText")
+        self.actual_odds = info.find_element(By.XPATH, "./span").get_attribute("innerText")
 
         # Get list of books
-        image_divs = info.find_elements(By.XPATH, "./div")[self.__top_or_bottom_bet].find_elements(By.XPATH, "./div")
-        for image_div in image_divs:
-            img_elem = image_div.find_elements(By.XPATH, "./a/img")
-            if len(img_elem):
-                book_name = img_elem[0].get_attribute("alt")
-            else:
-                book_name = image_div.find_element(By.XPATH, "./img").get_attribute("alt")
-            if book_name != "OddsJam":
+        image_divs = info.find_elements(By.XPATH, "./div/div")
+
+        for image in image_divs:
+            book = image.find_elements(By.XPATH, "./img")
+            if not len(book):
+                book = image.find_elements(By.XPATH, "./a/img")
+            book_name = book[0].get_attribute("alt")
+            if book_name != "OddsJam" and book_name not in self.sportsbooks:
                 self.sportsbooks.append(book_name)
 
     def __get_market_odds(self):
-        odds = self.__bet_row_elements[self.__element_map["market_odds"]].find_elements(By.XPATH, "./a/span/span")
-        self.market_odds = odds[self.__top_or_bottom_bet].get_attribute("innerText")
+        odds = self.__bet_info_elements[self.__top_or_bottom_bet][self.__bet_element_map["market_info"]].find_element(By.XPATH, "./div/span")
+        self.market_odds = odds.get_attribute("innerText")
 
     def __get_no_vig_odds(self):
-        odds = self.__bet_row_elements[self.__element_map["no_vig_odds"]].find_elements(By.XPATH, "./a/span/span")
-        self.no_vig_odds = odds[self.__top_or_bottom_bet].get_attribute("innerText")
+        odds = self.__bet_info_elements[self.__top_or_bottom_bet][self.__bet_element_map["no_vig_odds"]].find_element(By.XPATH, "./div/span")
+        self.no_vig_odds = odds.get_attribute("innerText")
 
     def __get_recommended_bet_size(self):
         bet_midpoint = (self.__max_bet - self.__min_bet) / 2
@@ -192,15 +208,10 @@ class Bet:
         ])
 
     def gather_rest_information(self):
-        self.game_date = self.__convert_date(self.__get_inner_text("game_date"))
+        self.game_date, self.game_time = self.__convert_date(self.__get_inner_text("game_date"))
         self.__get_event_info()
         self.market = self.__get_inner_text("market")
-        self.__get_bet_description()
-        self.__get_sportsbook_info()
-        self.__get_market_odds()
-        self.__get_no_vig_odds()
-        self.__get_recommended_bet_size()
-        self.__get_bet_id()
+        self.__get_bet_info()
 
 
 def send_to_discord(msg):
@@ -244,16 +255,17 @@ def check_bets(bet_rows):
         if percent < MIN_ACCEPTABLE_PERCENT:
             break
 
-        desired = desired_bet(width, percent)
-
         # Check only percent/width, then decide to gather the rest of the information
-        if desired:
+        if desired_bet(width, percent):
             bet.gather_rest_information()
             if bet.bet_id not in alert_log:
                 alert_log[bet.bet_id] = bet.game_date
                 bet_msg = bet.msg()
                 send_to_discord(bet_msg)
                 print(f"Alerted bet! Details below:\n{bet_msg}\n")
+
+        # Free memory
+        del bet
 
 
 def clean_alert_log():
@@ -308,7 +320,8 @@ def make_oj_driver(driver):
         while driver is None:
             try:
                 time.sleep(5)
-                driver = webdriver.Chrome("drivers/chromedriver_win.exe", desired_capabilities=caps, options=chrome_options)
+                driver = webdriver.Chrome("drivers/chromedriver_win.exe", desired_capabilities=caps,
+                                          options=chrome_options)
                 print("Driver created...")
             except selenium.common.exceptions.InvalidArgumentException as ex:
                 print("Wasn't able to use the chrome user profile, trying again in 30 seconds...")
@@ -349,8 +362,7 @@ def refresh_table(refresh_button):
 
 
 def read_new_bets(driver):
-    rows = driver.find_element(By.TAG_NAME, "table").find_elements(By.TAG_NAME, "tr")
-    rows.remove(rows[0])
+    rows = driver.find_element(By.CLASS_NAME, "overflow-scroll").find_elements(By.XPATH, "./a")
     print("Starting 'check_bets' operation...")
     start_time = time.time()
 
@@ -369,11 +381,9 @@ def read_new_bets(driver):
     processes = []
     n_processes = 10
     process_size = math.ceil(len(rows) / n_processes)
-    # print(f"Total = {len(rows)}")
     for i in range(1, n_processes + 1):
         start = (i - 1) * process_size
         end = i * process_size if len(rows) >= i * process_size else len(rows)
-        # print(f"Process {i} = {len(rows[start:end])}")
         p = multiprocessing.Process(target=check_bets(rows[start:end]))
         p.start()
         processes.append(p)
@@ -385,7 +395,7 @@ def read_new_bets(driver):
 
 def check_for_clear_cache(driver, refresh_button):
     global refresh_count
-    if refresh_count > 10:
+    if refresh_count > 25:
         driver, refresh_button = make_oj_driver(driver)
         refresh_count = 0
     return driver, refresh_button
